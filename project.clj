6492d7fe92d80e813d4153f6d9180376ad85177a
Bill <bill.burcham@gmail.com>
2015-03-23 11:05:15 -0700

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject progress-light "0.1.0"
+(defproject progress-light "0.1.1"
   :description "throttles clj-progress"
   :url "https://github.com/Bill/progress-light"
   :license {:name "Eclipse Public License"
